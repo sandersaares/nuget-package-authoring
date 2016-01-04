816c3772009e82765ecd73e5691e192631cb187c
Sander Saares <saares@axinom.com>
2016-01-04 15:55:01 +0200

    Added XAML user control support to multi-surface library example.

---
+++ MultiSurfaceLibrary/MyUwpLibrary/HashControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace ExampleLibrary
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using Windows.UI.Xaml.Controls;
+
+	/// <summary>
+	/// A control that displays a hash. Just used to add flavor to the example since exporting XAML controls creates some
+	/// more files in the output directory that must also be properly packaged together with the assembly file itself.
+	/// </summary>
+	public sealed partial class HashControl : UserControl
+	{
+		/// <summary></summary>
+		public HashControl()
+		{
+			this.InitializeComponent();
+
+			OutputLabel.Text = HashHelper.CalculateStringHash("hello").ToString();
+		}
+	}
+}
