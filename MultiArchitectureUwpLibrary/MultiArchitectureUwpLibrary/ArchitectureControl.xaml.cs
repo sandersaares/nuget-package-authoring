2b1d87a82457b15aea43872b7c2ef85b0b500a28
Sander Saares <saares@axinom.com>
2016-01-04 16:45:00 +0200

    Bugfix: multi-architecture sample did not support XAML exports.

---
+++ MultiArchitectureUwpLibrary/MultiArchitectureUwpLibrary/ArchitectureControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MultiArchitectureUwpLibrary
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using Windows.UI.Xaml.Controls;
+
+	/// <summary>
+	/// A control that displays a hash. Just used to add flavor to the example since exporting XAML controls creates some
+	/// more files in the output directory that must also be properly packaged together with the assembly file itself.
+	/// </summary>
+	public sealed partial class ArchitectureControl : UserControl
+	{
+		/// <summary></summary>
+		public ArchitectureControl()
+		{
+			this.InitializeComponent();
+
+			Label.Text = ArchitectureDetector.GetCurrentArchitecture();
+		}
+	}
+}
