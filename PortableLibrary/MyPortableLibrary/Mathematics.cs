769ffe545c5b10c21e4c415d0656a93079a161da
Sander Saares <saares@axinom.com>
2016-01-05 09:53:18 +0200

    Added simple portable library example.

---
+++ PortableLibrary/MyPortableLibrary/Mathematics.cs
@@ -0,0 +1,15 @@
+﻿namespace MyPortableLibrary
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Numerics;
+
+	public static class Mathematics
+	{
+		public static Vector4 Transform(Vector4 v)
+		{
+			return v;
+		}
+	}
+}
