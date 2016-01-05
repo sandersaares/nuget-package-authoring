0bb8524656a47f8c657434deeac25de6110a3264
Sander Saares <saares@axinom.com>
2016-01-05 13:31:03 +0200

    Added SimpleUwpLibrary sample.

---
+++ SimpleUwpLibrary/SimpleUwpLibrary/HelloWorld.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace SimpleUwpLibrary
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using Windows.UI.Xaml.Controls;
+	using Newtonsoft.Json;
+
+	public sealed partial class HelloWorld : UserControl
+	{
+		public HelloWorld()
+		{
+			this.InitializeComponent();
+
+			var json = JsonConvert.SerializeObject(new
+			{
+				Text = "Hello, world!"
+			});
+
+			Label.Text = json;
+		}
+	}
+}
