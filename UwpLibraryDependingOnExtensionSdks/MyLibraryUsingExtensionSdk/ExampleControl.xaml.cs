a480e8512bbabb4710bb60d3cb3a5b62fd29e141
Sander Saares <saares@axinom.com>
2016-01-05 14:23:56 +0200

    Bugfix: we were including libraries from extension SDK in the NuGet package. This was not right.
    Also included XAML for better example.

---
+++ UwpLibraryDependingOnExtensionSdks/MyLibraryUsingExtensionSdk/ExampleControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MyLibraryUsingExtensionSdk
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using Windows.UI.Xaml.Controls;
+
+	/// <summary>
+	/// Just a dummy control to ensure some XAML is generated and the example is whole.
+	/// </summary>
+	public sealed partial class ExampleControl : UserControl
+	{
+		public ExampleControl()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
